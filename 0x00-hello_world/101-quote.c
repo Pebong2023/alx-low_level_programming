0d456b7df7787e1e946994f89c34421ef294c6c2
Ebong Christopher Patrick <ebongchris21@gmail.com>
2023-03-16 21:59:44 -0700

    qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * DEcription: print a quote using write function
+ * size_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error)
+ */
+
+int main(void)
+{
+	write(2, "and the piece of art is useful -Dora Korpar, 2015-10-19, \n"59);
+	return (1);
+}
