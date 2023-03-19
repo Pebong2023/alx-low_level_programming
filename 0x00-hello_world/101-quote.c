81dbf92eaa7829b3380425f8ee8e5bb4b3cb1410
Ebong Christopher Patrick <ebongchris21@gmail.com>
2023-03-19 07:12:39 -0700

    unix is basically simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-	write(2, "and the piece of art is useful" - Dora Korpar, 2015-10-19, \n", 59);
+f:puts("and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
