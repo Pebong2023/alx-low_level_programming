1f9024eaa00d20ebb34def01bc8201655e27f40b
Ebong Christopher Patrick <ebongchris21@gmail.com>
2023-03-19 17:00:19 -0700

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,15 @@ int i;
 int j;
 
 
-for (i = 0; i <= 100; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = 0; j <= 100; j++)
+for (j = 0; j < 100; j++)
 {
 
 if (i <  j)
 {
 putchar((i / 10) + 48);
-putchar((1 % 10) + 48);
+putchar((j % 10) + 48);
 putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
