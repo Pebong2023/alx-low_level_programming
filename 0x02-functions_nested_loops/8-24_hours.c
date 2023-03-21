fbf810e9249766407612ac02f92361f322e719b0
Pebong2023 <ebongchris21@gmail.com>
2023-03-21 23:57:11 +0100

    today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,12 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	while (i <= 24)
+	i = 0;
+
+	while (i < 24)
 	{
 		j = 0;
-		while (j <= 60)
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
