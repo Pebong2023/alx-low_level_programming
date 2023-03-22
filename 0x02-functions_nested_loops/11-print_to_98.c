9a86f5c0b913f0689cd420d91aa6b7eeaa2aa378
Pebong2023 <ebongchris21@gmail.com>
2023-03-22 02:31:59 +0100

    time table starting from 0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ if (n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
-if (i != 98)
+i:f (i != 98)
 printf("%d, ", i);
 else if (i == 98)
 printf("%d\n", i);
@@ -21,7 +21,7 @@ printf("%d\n", i);
 }
 else if (n >= 98)
 {
-for (j = n; j > 98; j--)
+for (j = n; j >= 98; j--)
 {
 if (j == 98)
 printf("%d, ", j);
