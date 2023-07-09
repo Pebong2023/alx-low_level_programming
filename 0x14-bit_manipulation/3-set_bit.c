79bb9b3c23a2c9b50880404c6e5b14acb5e20ff6
Pebong2023 <ebongchris21@gmail.com>
2023-07-09 18:30:53 +0100

    function that set value to bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- *get_bit - returns the value of a bit at an index in a decimal number
+ *set_bit - returns the value of a bit at an index in a decimal number
  *@n: number to search
  *@index: index of the bit
  *
- *Return: value of the bit
+ *Return: 1 for success, -1 for failure
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bit_val;
-
 	if (index > 63)
-	return (-1);
+		return (-1);
 
-	bit_val = (n >> index) & 1;
+	*n = ((1UL << index) | *n);
 
-	return (bit_val);
+	return (1);
 }
